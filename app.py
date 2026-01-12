88e3d430227fa838937de8d09f65ba1b61a7a23d
Ivo-velev <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>
2026-01-12 15:22:47 +0000

    added db url to app.py and update requirements

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, jsonify
+import os
+from extensions import db
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
+if not app.config.get("SQLALCHEMY_DATABASE_URI"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("db_url")
+
+db.init_app(app)
+
+load_dotenv
 
 @app.route("/test")
 def test():
