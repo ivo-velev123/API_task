6c5be4edbec7b6b016d83043396aa36b40b7013f
Ivo-velev <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>
2026-01-12 14:34:46 +0000

    created models for coin, duties and ksbs

--- models.py
+++ models.py
@@ -1,4 +1,19 @@
 from extensions import db
 from sqlalchemy.orm import Mapped, mapped_column
-from datetime import date
+import uuid
+
+class Coin(db.Model):
+    __tablename__ = "coins"
+    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    coin_name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+class Duty(db.Model):
+    __tablename__ = "duties"
+    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    duty_name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+class Ksb(db.Model):
+    __tablename__ = "KSB"
+    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    ksb_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
