d8cbee5b6ff40d8182957da89034eabc48a09de4
Ivo-velev <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>
2026-01-12 16:03:01 +0000

    fixed uuid

--- models.py
+++ models.py
@@ -4,16 +4,16 @@ import uuid
 
 class Coin(db.Model):
     __tablename__ = "coins"
-    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
     coin_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
 class Duty(db.Model):
     __tablename__ = "duties"
-    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
     duty_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
 class Ksb(db.Model):
     __tablename__ = "KSB"
-    id: Mapped[int] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid64))
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
     ksb_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
