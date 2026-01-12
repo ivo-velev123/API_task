61f3bc05e9b27398bda851028904f3e2b4deca8e
Ivo-velev <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>
2026-01-12 16:16:42 +0000

    added junction tables

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from extensions import db
+from sqlalchemy import Table, Column, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 import uuid
 
@@ -13,7 +14,20 @@ class Duty(db.Model):
     duty_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
 class Ksb(db.Model):
-    __tablename__ = "KSB"
+    __tablename__ = "ksbs"
     id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
     ksb_name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
+coins_duties = Table(
+    "coins_duties",
+    db.metdadata,
+    Column("coin_id", String, ForeignKey('coins.id'), primary_key=True),
+    Column("duty_id", String, ForeignKey('duty.id'), primary_key=True)
+)
+
+duties_ksbs = Table(
+    "duties_ksbs",
+    db.metadata,
+    Column("duty_id", String, ForeignKey("duties.id"), primary_key=True),
+    Column("ksb_id", String, ForeignKey("ksbs.id"), primary_key=True)
+)
