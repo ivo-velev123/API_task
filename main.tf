5664d954508387ac61b7d4d565c9f2b65876cbbc
Ivo-velev <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>
2026-02-02 09:32:10 +0000

    update security group since nuke

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = "subnet-ba3fc8d3"
-  vpc_security_group_ids = ["sg-0d951b9dddd0a3768"]
+  vpc_security_group_ids = ["sg-0213e780821464075"]
   key_name               = "terraform-key"
   user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
     instance_name      = "ivo-flask-api"
